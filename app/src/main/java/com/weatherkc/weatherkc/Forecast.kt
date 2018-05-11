16ca59477e0d36185a8c3d551c59bc9d87ab7935
Irma <irblancofdz@gmail.com>
2018-05-11 20:49:15 +0200

    Refactor MainActivity to ForecastActivity and add Forecast class

---
+++ app/src/main/java/com/weatherkc/weatherkc/Forecast.kt
@@ -0,0 +1,5 @@
+package com.weatherkc.weatherkc
+
+class Forecast(var maxTemp: Float, var minTemp: Float, var humidity: Float, var description: String, var icon: Int) {
+    
+}
--- app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
+++ app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class ForecastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
