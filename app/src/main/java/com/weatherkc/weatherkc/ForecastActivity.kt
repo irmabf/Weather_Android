aa44b221405d3359b4c5b8e6445e74509b7f155f
Irma <irblancofdz@gmail.com>
2018-05-11 22:41:01 +0200

    Complete MVC, model in view

--- app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
+++ app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
@@ -3,7 +3,9 @@ package com.weatherkc.weatherkc
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+
 import kotlinx.android.synthetic.main.activity_forecast.*
+import kotlin.math.min
 
 class ForecastActivity : AppCompatActivity() {
 
@@ -11,10 +13,24 @@ class ForecastActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
 
+        val forecast = Forecast(
+                maxTemp = 25f,
+                minTemp = 10f,
+                humidity = 35f,
+                description = "Soleado con alguna nube",
+                icon = R.drawable.ico_01)
+
+        setForecast(forecast)
+
     }
 
-    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
-        super.onSaveInstanceState(outState)
+    fun setForecast(forecast: Forecast){
+        forecast_image.setImageResource(forecast.icon)
+        forecast_description.text = forecast.description
+
+        maxTemp.text = getString(R.string.max_temp_format, forecast.maxTemp)
+        minTemp.text = getString(R.string.min_temp_format, forecast.minTemp)
+        humidity.text = getString(R.string.humidity_format, forecast.humidity)
     }
 
 }
