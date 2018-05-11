3359c9f2a4800d2e8086cb216b37aef5df770096
Irma <irblancofdz@gmail.com>
2018-05-11 21:09:49 +0200

    Rename activity_main to activity_forecast

--- app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
+++ app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
@@ -3,13 +3,13 @@ package com.weatherkc.weatherkc
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_forecast.*
 
 class ForecastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forecast)
 
 
         european_system_button?.setOnClickListener{
