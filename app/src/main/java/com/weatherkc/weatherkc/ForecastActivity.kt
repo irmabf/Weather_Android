ff1d0420393eca7b133efe6cfca7b8d0c69efeb8
Irma <irblancofdz@gmail.com>
2018-05-11 22:54:41 +0200

    Add companion object

--- app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
+++ app/src/main/java/com/weatherkc/weatherkc/ForecastActivity.kt
@@ -5,32 +5,39 @@ import android.os.Bundle
 import android.os.PersistableBundle
 
 import kotlinx.android.synthetic.main.activity_forecast.*
+import kotlin.math.max
 import kotlin.math.min
 
 class ForecastActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = ForecastActivity::class.java.canonicalName
+    }
+
+    var forecast: Forecast? = null
+        set(value) {
+            if (value != null){
+                forecast_image.setImageResource(value.icon)
+                forecast_description.text = value.description
+
+                maxTemp.text = getString(R.string.max_temp_format, value.maxTemp)
+                minTemp.text = getString(R.string.min_temp_format, value.minTemp)
+                humidity.text = getString(R.string.humidity_format, value.humidity)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
 
-        val forecast = Forecast(
+        forecast = Forecast(
                 maxTemp = 25f,
                 minTemp = 10f,
                 humidity = 35f,
                 description = "Soleado con alguna nube",
                 icon = R.drawable.ico_01)
 
-        setForecast(forecast)
 
-    }
-
-    fun setForecast(forecast: Forecast){
-        forecast_image.setImageResource(forecast.icon)
-        forecast_description.text = forecast.description
-
-        maxTemp.text = getString(R.string.max_temp_format, forecast.maxTemp)
-        minTemp.text = getString(R.string.min_temp_format, forecast.minTemp)
-        humidity.text = getString(R.string.humidity_format, forecast.humidity)
     }
 
 }
